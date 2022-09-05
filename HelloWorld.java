c6ce238c5e51432b6041af779d6970155aafb67e
ERICXUCHI <exc0000@163.com>
2022-09-05 10:45:56 +0800

    second version

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,28 @@
 import java.util.ArrayList;
 
-public class HelloWorld { static ArrayList<Integer> arrayList = new ArrayList<>();
+/**
+ * Test Example.
+ *
+ * @description: This is one simple example for test.
+ * @author: Eric
+ * @create: 2022-09-05
+ **/
 
-    public static void main(String[] args) {for
-    (int i = 0;
-    i < 100;
-    i++) {
-            arrayList.add(i);
-            arrayList.size();
-            arrayList.contains(i);
-        }
-        System.out.println("Hello");
+public class HelloWorld {
+  static ArrayList<Integer> arrayList = new ArrayList<>();
+
+  /**
+  * This method is the start of the program.
+
+  * @param args input para
+  */
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 100; i++) {
+      arrayList.add(i);
+      arrayList.size();
+      arrayList.contains(i);
     }
+    System.out.println("Hello");
+  }
 }
